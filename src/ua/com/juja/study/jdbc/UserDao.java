2a448b4466850794c951f3c433022874ae180da3
vviictoorr <victor.pidleteychuk@gmail.com>
2014-12-04 23:53:42 +0200

    extractUser()

---
+++ src/ua/com/juja/study/jdbc/UserDao.java
@@ -0,0 +1,26 @@
+package ua.com.juja.study.jdbc;
+
+/**
+ * Created by VICTOR on 04.12.2014.
+ */
+public class UserDao {
+//    public User getUserByEmail(String email){
+//
+//    }
+//
+//    public List<User> getAllUsers(){
+//
+//    }
+
+    public void addPhone(String phoneNumber, String email){
+
+    }
+
+    public void changePhone(String oldPhone, String newPhone){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Go");
+    }
+}
